af3b0e404f202831c6af1ecb35cec901a4021c03
Tim McNamara <26631157+TimMcN@users.noreply.github.com>
2026-03-27 15:07:02 +0000

    update train file with data generator

--- train.py
+++ train.py
@@ -25,15 +25,17 @@ from data import create_dataset
 from models import create_model
 from util.visualizer import Visualizer
 from util.util import init_ddp, cleanup_ddp
-
+from tim.generator import Diagonalized_Generator, Unordered_Generator, Lorentz_Generator
+import math
 
 if __name__ == "__main__":
     opt = TrainOptions().parse()  # get training options
     opt.device = init_ddp()
-    dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
-    dataset_size = len(dataset)  # get the number of images in the dataset.
+    #batch_size = 128
+    #dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
+    dataset_size = 30000  # get the number of images in the dataset.
     print(f"The number of training images = {dataset_size}")
-
+    generator = Lorentz_Generator(mode="diagonalized", dataset=9, padding = 25, n_range=(0.1, 2000, 256))
     model = create_model(opt)  # create a model given opt.model and other options
     model.setup(opt)  # regular setup: load and print networks; create schedulers
     visualizer = Visualizer(opt)  # create a visualizer that display/save images and plots
@@ -44,19 +46,17 @@ if __name__ == "__main__":
         epoch_iter = 0  # the number of training iterations in current epoch, reset to 0 every epoch
         visualizer.reset()
         # Set epoch for DistributedSampler
-        if hasattr(dataset, "set_epoch"):
-            dataset.set_epoch(epoch)
-
-        for i, data in enumerate(dataset):  # inner loop within one epoch
+        
+        for i in range(math.ceil(dataset_size/opt.batch_size)):  # inner loop within one epoch
             iter_start_time = time.time()  # timer for computation per iteration
             if total_iters % opt.print_freq == 0:
                 t_data = iter_start_time - iter_data_time
 
             total_iters += opt.batch_size
             epoch_iter += opt.batch_size
+            data = generator.generate_batch(opt.batch_size)
             model.set_input(data)  # unpack data from dataset and apply preprocessing
             model.optimize_parameters()  # calculate loss functions, get gradients, update network weights
-
             if total_iters % opt.display_freq == 0:  # display images on visdom and save images to a HTML file
                 save_result = total_iters % opt.update_html_freq == 0
                 model.compute_visuals()
@@ -67,6 +67,8 @@ if __name__ == "__main__":
                 t_comp = (time.time() - iter_start_time) / opt.batch_size
                 visualizer.print_current_losses(epoch, epoch_iter, losses, t_comp, t_data)
                 visualizer.plot_current_losses(total_iters, losses)
+                model.plot_1d_signals(visualizer, epoch)
+                
 
             if total_iters % opt.save_latest_freq == 0:  # cache our latest model every <save_latest_freq> iterations
                 print(f"saving the latest model (epoch {epoch}, total_iters {total_iters})")
