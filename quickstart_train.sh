2c2cf0ed0367e9f4486fd581dba5529aa2d339c1
Tim <timothyjmcnmara@gmail.com>
2026-03-27 15:02:26 +0000

    Init

---
+++ quickstart_train.sh
@@ -0,0 +1,2 @@
+python train.py --name train_2 --dataroot None --model pix2pix_mu --batch_size 128 --epoch_count 1 --output_nc 1 --input_nc 1 --no_html --update_html_freq 10000000000 --direction BtoA --lambda 500 --no_flip --preprocess none --print_freq 30208 --gan_mode lsgan
+
