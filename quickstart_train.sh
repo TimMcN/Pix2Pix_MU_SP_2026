8891d406343100bc4c94122317124b876ad2315c
Tim McNamara <26631157+TimMcN@users.noreply.github.com>
2026-04-30 13:30:18 +0100

    ft

--- quickstart_train.sh
+++ quickstart_train.sh
@@ -1,6 +1,6 @@
-python train.py --name timing_test --dataroot None --model pix2pix_mu --batch_size 5 --epoch_count 1 \
+python train.py --name train_tanh_d9_1000e_2_decay500_wganp --dataroot None --model pix2pix_mu --batch_size 256 --epoch_count 1 \
 --output_nc 1 --input_nc 1 --no_html --update_html_freq 10000000000 --direction BtoA --lambda 500 \
---no_flip --preprocess none --print_freq 1 --gan_mode lsgan --max_dataset_size 20 \
---dataset 9 --D_LR_Mul 0.1 --use_tensorboard --n_epochs 900 --lr 0.0002 --save_epoch_freq 100 \
---display_freq 20  --save_latest_freq 25
+--no_flip --preprocess none --print_freq 1 --gan_mode lsgan --max_dataset_size 10000 \
+--dataset 9 --D_LR_Mul 0.1 --use_tensorboard --n_epochs 500 --lr 0.0002 --save_epoch_freq 250 \
+--display_freq 50  --save_latest_freq 25 --n_epochs_decay 500
 
